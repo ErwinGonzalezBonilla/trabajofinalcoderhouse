1d0f58a01f86b79f08e8fd01b94c1571939b149f
Erwin Gonzalez <alexcultural@gmail.com>
2024-03-16 19:04:47 +0100

    renderizando la vista de detalles de productos

--- productos/urls.py
+++ productos/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name= 'productos'
 
 urlpatterns = [
+    path("<int:id>/", views.detalle, name='detalle_producto'),
     path("", views.listado)
 ]
--- productos/views.py
+++ productos/views.py
@@ -5,3 +5,8 @@ def listado(request):
     productos = Product.objects.all()
     context= {'productos': productos}
     return render(request, "product_list.html", context)
+
+def detalle(request, id):
+    producto = Product.objects.filter(id = id).first()
+    context= {'producto': producto}
+    return render(request, "product_detail.html", context)
