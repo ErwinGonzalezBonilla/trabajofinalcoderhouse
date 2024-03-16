264a77f1abf62f88b6be07fb97bf563f1252f4b3
Erwin Gonzalez <alexcultural@gmail.com>
2024-03-16 16:44:20 +0100

    Registrando modelo de producto

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    PRODUCT_TYPES = [
+        ("INSTRUMENTOS", "INSTRUMENTOS"),
+        ("ACCESORIOS", "ACCESORIOS")
+    ]
+    nombre= models.CharField(max_length=150)
+    marca= models.CharField(max_length=100)
+    modelo= models.CharField(max_length=100, null=True, blank=True)
+    descripcion= models.TextField(blank=True, default="", null=True)
+    tipo= models.CharField(max_length=50, choices=PRODUCT_TYPES )
+    categoria= models.CharField(max_length=150)
+    precio= models.FloatField(max_length=150)
+   # imagen= models.ImageField(upload_to="uploads/%Y/%m")
+    
